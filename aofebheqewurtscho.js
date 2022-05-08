2242446293d397d6fe7562370db89604acdc7f19
Maarcis <markbodonnell@gmail.com>
2022-05-08 20:10:55 +0100

    Add files via upload

---
+++ aofebheqewurtscho.js
@@ -0,0 +1,5 @@
+//'sdjalúsn hýse muchuo eiwurtsd þiqr fóon
+let count = 0;
+count = document.getElementsByTagName("b").length;
+let heading1 = document.createTextNode(count)
+document.getElementById("iwurtsbúch").appendChild(heading1);
---
+++ index.js
@@ -0,0 +1,15 @@
+var lanwurq = document.getElementById("lanwurq");
+var english = document.getElementById("english");
+
+english.style.display = "none"
+
+function showLanwurq() {
+    lanwurq.style.display = "inline"
+    english.style.display = "none"
+}
+
+function showEnglish() {
+    english.style.display = "inline"
+    lanwurq.style.display = "none"
+}
+
