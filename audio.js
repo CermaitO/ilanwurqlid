86f0e0d61381eb2ab6107a3c3eaf66fb714edad1
Maarcis <markbodonnell@gmail.com>
2022-12-18 09:07:31 +0000

    .

---
+++ audio.js
@@ -0,0 +1,289 @@
+let sijlafon = new Audio("sijlafon.mp3");
+
+document.getElementById('sijlafon').onclick = function() {
+  sijlafon.play();
+}
+
+let sibra = new Audio("sibra.mp3");
+
+document.getElementById('sibra').onclick = function() {
+  sibra.play();
+}
+
+let qijara = new Audio("qijara.mp3");
+
+document.getElementById('qijara').onclick = function() {
+  qijara.play();
+}
+
+let hos = new Audio("hos.mp3");
+
+document.getElementById('hos').onclick = function() {
+  hos.play();
+}
+
+let hós = new Audio("hós.mp3");
+
+document.getElementById('hós').onclick = function() {
+  hós.play();
+}
+
+let bobhe = new Audio("bobhe.mp3");
+
+document.getElementById('bobhe').onclick = function() {
+  bobhe.play();
+}
+let bóbhe = new Audio("bóbhe.mp3");
+
+document.getElementById('bóbhe').onclick = function() {
+  bóbhe.play();
+}
+let hus = new Audio("hus.mp3");
+
+document.getElementById('hus').onclick = function() {
+  hus.play();
+}
+let hús = new Audio("hús.mp3");
+
+document.getElementById('hús').onclick = function() {
+  hús.play();
+}
+let hon = new Audio("hon.mp3");
+
+document.getElementsByClassName('hon')[0].onclick = function() {
+  hon.play();
+}
+document.getElementsByClassName('hon')[1].onclick = function() {
+  hon.play();
+}
+document.getElementsByClassName('hon')[2].onclick = function() {
+  hon.play();
+}
+document.getElementsByClassName('hon')[3].onclick = function() {
+  hon.play();
+}
+document.getElementsByClassName('hon')[4].onclick = function() {
+  hon.play();
+}
+
+
+let hón = new Audio("hón.mp3");
+
+document.getElementById('hón').onclick = function() {
+  hón.play();
+}
+let hen = new Audio("hen.mp3");
+
+document.getElementById('hen').onclick = function() {
+  hen.play();
+}
+let hén = new Audio("hén.mp3");
+
+document.getElementById('hén').onclick = function() {
+  hén.play();
+}
+let grun = new Audio("grun.mp3");
+
+document.getElementById('grun').onclick = function() {
+  grun.play();
+}
+let grún = new Audio("grún.mp3");
+
+document.getElementById('grún').onclick = function() {
+  grún.play();
+}
+let gof = new Audio("gof.mp3");
+
+document.getElementById('gof').onclick = function() {
+  gof.play();
+}
+
+let góf = new Audio("góf.mp3");
+
+document.getElementById('góf').onclick = function() {
+  góf.play();
+}
+let go = new Audio("go.mp3");
+
+document.getElementById('go').onclick = function() {
+  go.play();
+}
+let gó = new Audio("gó.mp3");
+
+document.getElementById('gó').onclick = function() {
+  gó.play();
+}
+let goq = new Audio("goq.mp3");
+
+document.getElementById('goq').onclick = function() {
+  goq.play();
+}
+let góq = new Audio("góq.mp3");
+
+document.getElementById('góq').onclick = function() {
+  góq.play();
+}
+
+let gin = new Audio("gin.mp3");
+document.getElementsByClassName('gin')[0].onclick = function() {
+
+        gin.play();
+}
+document.getElementsByClassName('gin')[1].onclick = function() {
+
+        gin.play();
+}
+
+
+let gín = new Audio("gín.mp3");
+
+document.getElementById('gín').onclick = function() {
+  gín.play();
+}
+let ged = new Audio("ged.mp3");
+
+document.getElementById('ged').onclick = function() {
+  ged.play();
+}
+let géd = new Audio("géd.mp3");
+
+document.getElementById('géd').onclick = function() {
+  géd.play();
+}
+let fiq = new Audio("fiq.mp3");
+
+document.getElementById('fiq').onclick = function() {
+  fiq.play();
+}
+let fíq = new Audio("fíq.mp3");
+
+document.getElementById('fíq').onclick = function() {
+  fíq.play();
+}
+let en = new Audio("en.mp3");
+
+document.getElementById('en').onclick = function() {
+  en.play();
+}
+let én = new Audio("én.mp3");
+
+document.getElementById('én').onclick = function() {
+  én.play();
+}
+let eq = new Audio("eq.mp3");
+
+document.getElementById('eq').onclick = function() {
+  eq.play();
+}
+let éq = new Audio("éq.mp3");
+
+document.getElementById('éq').onclick = function() {
+  éq.play();
+}
+let dos = new Audio("dos.mp3");
+
+document.getElementById('dos').onclick = function() {
+  dos.play();
+}
+let dós = new Audio("dós.mp3");
+
+document.getElementById('dós').onclick = function() {
+  dós.play();
+}
+let cus = new Audio("cus.mp3");
+
+document.getElementById('cus').onclick = function() {
+  cus.play();
+}
+let cús = new Audio("cús.mp3");
+
+document.getElementById('cús').onclick = function() {
+  cús.play();
+}
+let con = new Audio("concon.mp3");
+
+document.getElementById('con').onclick = function() {
+  con.play();
+}
+let cón = new Audio("cón.mp3");
+
+document.getElementById('cón').onclick = function() {
+  cón.play();
+}
+let buch = new Audio("buch.mp3");
+
+document.getElementById('buch').onclick = function() {
+  buch.play();
+}
+let búch = new Audio("búch.mp3");
+
+document.getElementById('búch').onclick = function() {
+  búch.play();
+}
+let bred = new Audio("bred.mp3");
+
+document.getElementById('bred').onclick = function() {
+  bred.play();
+}
+let bréd = new Audio("bréd.mp3");
+
+document.getElementById('bréd').onclick = function() {
+  bréd.play();
+}
+let bos = new Audio("bos.mp3");
+
+document.getElementsByClassName('bos')[0].onclick = function() {
+  bos.play();
+}
+document.getElementsByClassName('bos')[1].onclick = function() {
+  bos.play();
+}
+
+let bós = new Audio("bós.mp3");
+
+document.getElementById('bós').onclick = function() {
+  bós.play();
+}
+let bon = new Audio("bon.mp3");
+
+document.getElementById('bon').onclick = function() {
+  bon.play();
+}
+let bón = new Audio("bón.mp3");
+
+document.getElementById('bón').onclick = function() {
+  bón.play();
+}
+let bi = new Audio("bi.mp3");
+
+document.getElementById('bi').onclick = function() {
+  bi.play();
+}
+let bí = new Audio("bí.mp3");
+
+document.getElementById('bí').onclick = function() {
+  bí.play();
+}
+let ben = new Audio("ben.mp3");
+
+document.getElementsByClassName('ben')[0].onclick = function() {
+  ben.play();
+}
+document.getElementsByClassName('ben')[1].onclick = function() {
+  ben.play();
+}
+document.getElementsByClassName('ben')[2].onclick = function() {
+  ben.play();
+}
+
+let bén = new Audio("bén.mp3");
+
+document.getElementsByClassName('bén')[0].onclick = function() {
+  bén.play();
+}
+document.getElementsByClassName('bén')[1].onclick = function() {
+  bén.play();
+}
+document.getElementsByClassName('bén')[2].onclick = function() {
+  bén.play();
+}
